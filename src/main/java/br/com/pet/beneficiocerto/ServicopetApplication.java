ed2be61847bd487cdb7b15edae7b3428122618a4
Nicolas Marzano <nicolasdutramarzano@gmail.com>
2019-05-13 16:47:27 -0300

    Projeto criado

---
+++ src/main/java/br/com/pet/beneficiocerto/ServicopetApplication.java
@@ -0,0 +1,13 @@
+package br.com.pet.beneficiocerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicopetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pet/beneficiocerto/ServicopetApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.pet.beneficiocerto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicopetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
